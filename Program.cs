775b755f636ec9556987880040c2371c096cd8f4
JFschmiechen <jfschmiechen@gmail.com>
2016-10-13 22:20:44 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,235 @@
+﻿/*
+Author: Schmiechen, James
+Description: My class programming assignment in C#. 
+    This program creates a player roster that allows coaches to manipulate and update the
+player list, ratings, jersey numbers, and to add new team members.
+*/
+
+/*
+	TODO:
+	- Fix bad input
+    - Simplify booleans
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpProject02
+{
+    class Program
+    {
+
+        static void badJersey(int x) // Checks for # not between 0 and 99
+        {
+            if (x > 99 || x < 0)
+            {
+                Console.WriteLine(x + " is an invalid value. Please try again. (0 - 99)\n");
+            }
+        }
+        static void badRating(int y) // Checks for # not between 1 and 9
+        {
+            if (y < 1 || y > 9)
+            {
+                Console.WriteLine(y + " is an invalid value. Please try again. (1 - 9)\n");
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            List<int> jersey = new List<int>(5);
+            List<int> rating = new List<int>(5);
+            char menuChoice;
+            int jerseyNum = 0, ratingNum = 0, tempJersey, playCount = 5;
+            string temp;
+
+            Console.WriteLine("Enter the jersey numbers (0 - 99) and ratings ( 1- 9) for five players." + Environment.NewLine);
+
+            for (int i = 1; i <= playCount; i++)
+            {
+                do
+                {
+
+                    Console.WriteLine("Enter player " + i + "'s jersey number: ");
+                    temp = Console.ReadLine();
+                    Int32.TryParse(temp, out jerseyNum);
+                    badJersey(jerseyNum); // Checks for bad input
+
+                } while (jerseyNum > 99 || jerseyNum < 0);
+
+                do
+                {
+
+                    Console.WriteLine("Enter Player " + i + "'s rating: ");
+                    temp = Console.ReadLine();
+                    Int32.TryParse(temp, out ratingNum);
+                    badRating(ratingNum);
+
+                } while ((ratingNum < 1 || ratingNum > 9));
+
+                jersey.Add(jerseyNum);
+                rating.Add(ratingNum);
+                Console.WriteLine(Environment.NewLine);
+            }
+            do
+            {
+                Console.WriteLine(Environment.NewLine + "MENU" + Environment.NewLine
+                + "a - Add player" + Environment.NewLine
+                + "d - Remove player" + Environment.NewLine
+                + "u - update player rating" + Environment.NewLine
+                + "r - Output players above a rating" + Environment.NewLine
+                + "o - Output roster" + Environment.NewLine
+                + "q - Quit" + Environment.NewLine
+                + Environment.NewLine + Environment.NewLine + "Choose an option: ");
+
+                menuChoice = Console.ReadKey().KeyChar;
+                switch (menuChoice)
+                {
+                case 'a': // Adds a player to the roster
+                        do
+                        {
+                            Console.WriteLine(Environment.NewLine + "Enter a new player's jersey number: ");
+                            temp = Console.ReadLine();
+                            Int32.TryParse(temp, out jerseyNum);
+                            badJersey(jerseyNum);
+
+                            for (int i = 0; i < playCount; i++)
+                            {
+                                if (jerseyNum == jersey.ElementAt(i))
+                                {
+
+                                    Console.WriteLine(jerseyNum + " is already being used. Please enter a new number." + Environment.NewLine);
+                                    break;
+
+                                }
+                            }
+
+                            tempJersey = jersey.Find(item => item == jerseyNum);
+
+                        } while ((jerseyNum > 99 || jerseyNum < 0) || (tempJersey == jerseyNum));
+                               
+                        do
+                        {
+
+                            Console.WriteLine(Environment.NewLine + "Enter the player's rating: ");
+                            temp = Console.ReadLine();
+                            Int32.TryParse(temp, out ratingNum);
+                            badRating(ratingNum);
+
+                        } while (ratingNum < 1 || ratingNum > 9);
+
+                        jersey.Add(jerseyNum);
+                        rating.Add(ratingNum);
+                        playCount++;
+                        break;
+
+                case 'd': // Deletes a player from the roster
+
+                        Console.WriteLine(Environment.NewLine + "Enter a jersey number: ");
+                        temp = Console.ReadLine();
+                        Int32.TryParse(temp, out jerseyNum);
+                        badJersey(jerseyNum);
+                        tempJersey = jersey.Find(item => item == jerseyNum);
+
+                                jersey.Remove(jerseyNum);
+                                rating.Remove(jerseyNum);
+                                playCount--;
+                            
+                             if (!(Convert.ToBoolean(tempJersey)) && (jerseyNum < 99 && jerseyNum > 0))
+                            {           
+                                Console.WriteLine(jerseyNum + " is not on the roster. Please try a different number." + Environment.NewLine);
+                                break;  // Specific error handling, returns error only if it cant find jerseyNum in jersey but still is in range (0 - 99)
+                            }
+                        
+                        break;
+
+                case 'r': // Outputs players above a rating
+
+                        do
+                        {
+
+                            Console.WriteLine(Environment.NewLine + "Enter a rating: ");
+                            temp = Console.ReadLine();
+                            Int32.TryParse(temp, out ratingNum);
+                            badRating(ratingNum);
+
+                        } while (ratingNum < 1 || ratingNum > 9);
+
+                        Console.WriteLine(Environment.NewLine + Environment.NewLine + "ABOVE" + Environment.NewLine + Environment.NewLine);
+
+                        for (int i = 0, n = 1; i < playCount; i++, n++)
+                        {
+                            if (rating.ElementAt(i) > ratingNum)
+                            {
+
+                                Console.WriteLine(Environment.NewLine + "Player " + n + " -- Jersey number: " + jersey.ElementAt(i) + ", rating: " + rating.ElementAt(i) + Environment.NewLine);
+
+                            }
+                        }
+                        break;
+
+                case 'u': // Update player rating
+
+                        do
+                        {
+
+                            Console.WriteLine(Environment.NewLine + "Enter a jersey number: ");
+                            temp = Console.ReadLine();
+                            Int32.TryParse(temp, out jerseyNum);
+                            badJersey(jerseyNum);
+
+                            tempJersey = jersey.Find(item => item == jerseyNum);
+                            if (tempJersey != jerseyNum)
+                            {
+                                Console.WriteLine(jerseyNum + " is not part of the roster. Please try again." + Environment.NewLine);
+                            }
+
+                        } while (tempJersey != jerseyNum);
+                        do
+                        {
+
+                            Console.WriteLine(Environment.NewLine + "Enter a new rating for the player: ");
+                            temp = Console.ReadLine();
+                            Int32.TryParse(temp, out ratingNum);
+                            badRating(ratingNum);
+
+                        } while (ratingNum < 1 || ratingNum > 9);
+
+                        for (int i = 0; i < playCount; i++)
+                        {
+                            if (jerseyNum == jersey.ElementAt(i))
+                            {
+                                rating[i] = ratingNum;
+                            }
+                        }
+                        break;
+
+                case 'o': // Output roster
+
+                        Console.WriteLine(Environment.NewLine);
+
+                        for (int i = 0; i < playCount; i++)
+                        {
+                            Console.WriteLine("Player " + (i + 1) + " -- Jersey number: " + jersey.ElementAt(i) + ", Rating: " + rating.ElementAt(i) + Environment.NewLine);
+                        }
+
+                        Console.WriteLine(Environment.NewLine);
+                        break;
+
+                case 'q': // Quit
+
+                        break;
+
+                default:
+
+                        Console.WriteLine(menuChoice + " is not a menu choice. Please try again." + Environment.NewLine);
+                        break;
+                }
+            } while (menuChoice != 'q');
+        }
+    }
+}
--- program02.cpp
+++ program02.cpp
@@ -1,154 +1,222 @@
 /*
 Author: Schmiechen, James
-Group members: Lastname, Firstname; Lastname, Firstname; Lastname, Firstname;
+Group members: Saleh, Zainab; Sherlock, Alec; Lastname, Firstname;
 Assignment: Programming Assignment Two
 Course: COMP B12
 Instructor: Richard Miles
-Date submitted: 2106/09/??
+Date submitted: 2016/12/10
 */
 
-// This program allows coaches to maintain a small roster of players.
+/*
+	TODO:
+	-Code might be over commented
+	-Fix bad input ( case 'u', 
+*/
 
 #include <iostream>
 #include <vector>
+#include <sstream> // Allows cin to read strings
+
 using namespace std;
 
+void badJersey(int x) { // Checks for jersey # not between 0 and 99
+
+	if (x > 99 || x < 0) { // || == or
+		cout << x << " is an invalid value. Please try again. (0 - 99)" << endl;
+		
+	}
+}
+
+void badRating(int y) { // Checks for rating # not between 1 and 9
+
+	if (y < 1 || y > 9) {
+		cout << y << " is an invalid value. Please try again. (1 - 9)" << endl;
+		
+	}
+}
+
 int menuFunction() {	// Outputs menu
-	cout << endl << "MENU" << endl << "a - Add player" << endl << "d - Remove player" << endl << "u - update player rating" << endl << "r - Output players above a rating" << endl << "o - Output roster" << endl << "q - Quit" << endl;
+	cout << endl << "MENU" << endl;
+	cout << "a - Add player" << endl;
+	cout << "d - Remove player" << endl;
+	cout << "u - update player rating" << endl;
+	cout << "r - Output players above a rating" << endl;
+	cout << "o - Output roster" << endl;
+	cout << "q - Quit" << endl;
 	cout << endl << endl << "Choose an option: ";
 	return 0;
 }
 
 int main() {
 	vector<int> jersey, rating;
-	vector<char> menuList;
-	int jerseyNum = 0, ratingNum = 0;
-	char menuChoice;
+	int jerseyNum = 0, ratingNum = 0, playCount = 5;
+	string temp;
+	
 
 	cout << "Enter the jersey numbers (0 - 99) and ratings (1 - 9) for five players." << endl;	// Prompt
 
-	for (int i = 1; i <= 5; i++) {
-		do		// Repeats current iteration to allow correction
+	for (int i = 1; i <= playCount; i++) {
+		do		
 		{
-			cout << "Enter player " << i << "'s jersey number: ";	// Collects input
+
+			cout << "Enter player " << i << "'s jersey number: ";	// Collects input and passes it to badJersey
 			cin >> jerseyNum;
-			if (jerseyNum > 99 || jerseyNum < 0) {
-				cout << jerseyNum << " is an invalid value. Please try again. (0 - 99)" << endl;
-			}
-		} while (jerseyNum > 99 || jerseyNum < 0);
+			badJersey(jerseyNum); // Checks for # not betwenn 0 and 99
+
+		} while (jerseyNum > 99 || jerseyNum < 0);	// Repeats current iteration to allow correction
 
 		do
 		{
+
 			cout << endl << "Enter player " << i << "'s rating: ";
 			cin >> ratingNum;
-			if (ratingNum < 1 || ratingNum > 9) {
-				cout << ratingNum << " is an invalid value. Please try again. (1 - 9)" << endl;
-			}
-		} while ((ratingNum < 1 || ratingNum > 9));
+			badRating(ratingNum);
+
+		} while (ratingNum < 1 || ratingNum > 9);
 
 		jersey.push_back(jerseyNum);	// Adds input to end of vectors
 		rating.push_back(ratingNum);
 		cout << endl;
 	}
+
 	do
 	{
-		menuFunction();
-		cin >> menuChoice;
-		switch (menuChoice) {
+
+		menuFunction();	// Prints menu
+		cin >> temp;
+		switch (temp[0]) {	// Uses first letter of input as choice
+
 		case 'a': // Add player
-			do
-			{
-				cout << "Enter a new player's jersey number: ";
-				cin >> jerseyNum;
-				if (jerseyNum > 99 || jerseyNum < 0) {
-					cout << jerseyNum << " is an invalid value. Please try again. (0 - 99)" << endl;
-				}
-				for (int i = 0; i < 5; i++) {
-					if (jerseyNum == jersey.at(i)) {
-						cout << jerseyNum << " is already being used. Please enter a new number." << endl;
+
+				do
+				{
+
+					cout << "Enter a new player's jersey number: ";
+					if (!(cin >> jerseyNum)) {
+						cin.clear();
+						cout << "Please enter a numeric value." << endl;
+						break;
 					}
-				}
-			} while ((jerseyNum > 99 || jerseyNum < 0));
-			do
-			{
-				cout << "Enter the player's rating: ";
-				cin >> ratingNum;
-				if (ratingNum < 1 || ratingNum > 9) {
-					cout << ratingNum << " is an invalid value. Please try again. (1 - 9)" << endl;
-				}
-			} while ((ratingNum < 1 || ratingNum > 9));
+					
+					badJersey(jerseyNum); // Checks for # not between 0 and 99
+					
+					for (int i = 0; i < playCount; i++) {
 
-			jersey.push_back(jerseyNum);	// Adds input to end of vectors
-			rating.push_back(ratingNum);
-			break;
+						if (jerseyNum == jersey.at(i)) { // Checks roster for matching jerseyNum
+							cout << jerseyNum << " is already being used. Please enter a new number." << endl;
+							break;
 
-		case 'd': // Remove player
-			cout << "Enter a jersey number: ";
-			cin >> jerseyNum;
-			for (int i = 0; i <= 5; i++) {
-				if (jersey.at(i) == jerseyNum) {
-					jersey.erase(jersey.begin() + i);
-					rating.erase(rating.begin() + i);
+						}
+					}
+					
+				} while ((find(jersey.begin(), jersey.end(), jerseyNum) != jersey.end()) || (jerseyNum > 99 || jerseyNum < 0));
+						// find returns true if it can find jerseyNum in jersey, if true the loop repeats.
+				do
+				{
+					 
+					cout << "Enter the player's rating: ";
+					cin >> ratingNum;
+					badRating(ratingNum); // Checks for # not between 1 and 9
+
+				} while (ratingNum < 1 || ratingNum > 9);
+
+				jersey.push_back(jerseyNum);	// Adds input to end of vectors
+				rating.push_back(ratingNum);
+				playCount++;	// Allows player roster to grow
+				break;
+
+		case 'd': // Remove player 
+
+					cout << "Enter a jersey number: ";
+					cin >> jerseyNum;
+					badJersey(jerseyNum);
+
+					for (int i = 0; i < playCount; i++) {
+
+							if (jersey.at(i) == jerseyNum) {
+								jersey.erase(jersey.begin() + i);	// Removes the value at index i
+								rating.erase(rating.begin() + i);
+								playCount--;
+								break;
+							
+							}
+							else if ((find(jersey.begin(), jersey.end(), jerseyNum) == jersey.end()) && (jerseyNum < 99 && jerseyNum > 0)) {
+								cout << jerseyNum << " is not on the roster. Please try a different number." << endl;
+								break;	// Specific error return if it can not find jerseyNum in jersey, but it is still a valid Jersey #
+							}
+					}
+					
 					break;
-				}
-				else { continue; }
-			}
-			break;
 
 		case 'r': // Output players above a rating
-			do
-			{
-				cout << "Enter a rating: ";
-				cin >> ratingNum;
-				if (ratingNum < 1 || ratingNum > 9) {
-					cout << ratingNum << " is an invalid value. Please try again. (1 - 9)" << endl;
-				}
-			} while ((ratingNum < 1 || ratingNum > 9));
 
-			cout << endl << endl << "ABOVE " << ratingNum << endl << endl;
-			for (int i = 0, n = 1; i < 5; i++, n++) {
-				if (rating.at(i) > ratingNum) {
-					cout << "Player " << n << " -- Jeresy number: " << jersey.at(i) << ", rating: " << rating.at(i) << endl;
+				do
+				{
+
+					cout << "Enter a rating: ";
+					cin >> ratingNum;
+					badRating(ratingNum);	// Checks for # not between 1 and 9
+
+				} while (ratingNum < 1 || ratingNum > 9);
+
+
+				cout << endl << endl << "ABOVE " << ratingNum << endl << endl;
+
+				for (int i = 0, n = 1; i < playCount; i++, n++) { // Searches vector for values higher than ratingNum
+					if (rating.at(i) > ratingNum) {		  // n keeps track of player #, and i connects ratingNum and jerseyNum
+
+						cout << "Player " << n << " -- Jersey number: " << jersey.at(i) << ", rating: " << rating.at(i) << endl;
+					}
 				}
-			}
-			break;
+
+				break;
 
 		case 'u': // Update player rating
-			do
-			{
-				cout << "Enter a jersey number: ";
-				cin >> jerseyNum;
-				if (find(jersey.begin(), jersey.end(), jerseyNum) == jersey.end())
-					cout << jerseyNum << " is not part of the roster. Please try again." << endl;
-			} while (find(jersey.begin(), jersey.end(), jerseyNum) == jersey.end());
-			do
-			{
-				cout << "Enter a new rating for player: ";
-				cin >> ratingNum;
-				if (ratingNum < 1 || ratingNum > 9) {
-					cout << ratingNum << " is an invalid value. Please try again. (1 - 9)" << endl;
-				}
-			} while ((ratingNum < 1 || ratingNum > 9));
 
-			for (int i = 0; i <= 5; i++) {
-				if (jerseyNum == jersey.at(i)) {
-					rating.at(i) = ratingNum;
-					break;
+				do
+				{
+
+					cout << "Enter a jersey number: ";
+					cin >> jerseyNum;
+
+					if (find(jersey.begin(), jersey.end(), jerseyNum) == jersey.end())	// Searches the whole vector for jerseyNum
+
+						cout << jerseyNum << " is not part of the roster. Please try again." << endl; // Error if it cant find jerseyNum
+
+				} while (find(jersey.begin(), jersey.end(), jerseyNum) == jersey.end());	// Repeat while it cant find jerseyNum
+				do
+				{
+
+					cout << "Enter a new rating for player: ";
+					cin >> ratingNum;
+					badRating(ratingNum);	// Checks for # not between 1 and 9
+
+				} while (ratingNum < 1 || ratingNum > 9);
+
+				for (int i = 0; i <= playCount; i++) {
+					if (jerseyNum == jersey.at(i)) { // Searches for index of jerseyNum, and uses that same index to update rating
+						rating.at(i) = ratingNum;
+						break;
+					}
 				}
-			}
-			break;
+
+				break;
 
 		case 'o': // Output roster
-				for (int i = 0; i < 5; i++) {
+
+				for (int i = 0; i < playCount; i++) {
 					cout << "Player " << i + 1 << " -- Jersey number: " << jersey.at(i) << ", Rating: " << rating.at(i) << endl;
 				}
 				break;
 
 		case 'q': // Quit
-			break;
+
+				exit(0);
 
 		default:
-			cout << "That is not a menu choice. Please try again." << endl;
-		}
-	} while (menuChoice != 'q');
-}
+
+				cout << temp << " is not a menu choice. Please try again." << endl;	// Error if input is not a menu choice
+
+			}
+		} while (temp[0] != 'q');	// Repeats the switch until you want to quit
+	}
